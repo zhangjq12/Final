3a4e94d7121bdc204870cabb7e5a7e8af4bf57f8
zhangjq12 <jzhan32@stevens.edu>
2019-11-22 16:21:28 -0500

    11

--- final/routes/order.js
+++ final/routes/order.js
@@ -4,13 +4,21 @@ const data = require("../data");
 const tabs = data.tabs;
 const users = data.users;
 const head = require("./head");
-const comments = data.comments;
 const authentication = require("./authentication");
 const xss = require('xss');
 const ObjectID = require('mongodb').ObjectID;
 
 router.get("/:id", async (req, res) => {
-    
+    const Head = await head(req);
+    const tabid = xss(req.params.id);
+    try {
+        const data = await tabs.getId(tabid);
+        console.log(2);
+        res.render("construct/order", {title: "Order " + data[0]["tabName"], status: Head});
+    }
+    catch(e) {
+        res.render("construct/error", {title: "Error!", status: Head});
+    }
 })
 
 module.exports = router;
